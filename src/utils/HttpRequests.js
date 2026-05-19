00475f0e53f4a49685d59378dc34d6a4bb458164
khoiDuytran <khoitranduy9404@gmail.com>
2026-05-19 10:08:14 +0700

    fix error api

--- src/utils/HttpRequests.js
+++ src/utils/HttpRequests.js
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 const HttpRequest = axios.create({
   // baseURL: "http://localhost:8080/api",
-  baseURL: "https://pharmacy-webapp-backend-nh7b.onrender.com/v3/api-docs",
+  baseURL: "https://pharmacy-webapp-backend-nh7b.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
